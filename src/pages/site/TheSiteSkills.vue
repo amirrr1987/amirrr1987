64978ae43c11e51455d052c85f296ad45ff9229b
amirrr1987 <maghami.a1987@gmail.com>
2022-05-27 13:08:38 +0430

    fix: TheSiteSkills
    - fix resposive layout
    - add transition effect

--- src/pages/site/TheSiteSkills.vue
+++ src/pages/site/TheSiteSkills.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="h-full grid grid-cols-2 grid-rows-3 p-5">
-    <div class="row-span-3 flex justify-center items-center">
+  <div class="h-full grid grid-cols-2 grid-rows-[1fr,max-content,1fr] lg:grid-rows-[2fr,1fr] p-5">
+
+    <div class="row-start-1 row-end-2 col-start-1 col-end-3 lg:col-start-2 lg:col-end-3">
+      <ApexChart :chartData="chartData" />
+    </div>
+
+    <div class="row-start-3 row-end-4 col-start-1 col-end-3 lg:row-start-1 lg:row-end-3 lg:col-end-2 flex justify-center items-center">
       <div
-        class="grid grid-cols-6 grid-rows-6 gap-3 aspect-square -rotate-45 w-9/12"
+        class="grid grid-cols-6 grid-rows-6 gap-3 aspect-square lg:-rotate-45 w-9/12 row-start-2 "
       >
         <template v-for="(item, index) in skills" :key="index">
           <button
@@ -10,24 +15,25 @@
             :class="item.position"
             @click="setData(item.name)"
           >
-            <div class="rotate-45">{{ item.name }}</div>
+            <div class="lg:rotate-45">{{ item.name }}</div>
           </button>
         </template>
       </div>
     </div>
-    <div class="row-span-2">
-      <ApexChart :chartData="chartData" />
-    </div>
-    <div class="relative overflow-hidden">
-      <transition name="fade" mode="out-in">
+
+    <div class="row-start-2 row-end-3 col-start-1 col-end-3 lg:col-start-2 lg:col-end-3 relative overflow-hidden">
+      <transition name="list" mode="out-in">
+        
         <div
-          v-show="showCaption"
+          v-if="showCaption"
           class="caption"
-          :class="showCaption ? '!translate-x-0' : 'translate-x-full'"
         >
-          <h3>Title: {{ targetSkill.name }}</h3>
-          <p class="e">detial: {{ targetSkill.caption }}</p>
-          <div>deuration : {{ targetSkill.duration }}</div>
+        <div class=""><span class="mr-2">skill:</span><span class="text-[#42b883]">{</span></div>
+            <div class="pl-10 grid grid-cols-[max-content,1fr] gap-x-2"><span class="">title: </span>"{{ targetSkill.name }}",</div>
+            <div class="pl-10 grid grid-cols-[max-content,1fr] gap-x-2"><span class="">detial: </span>"{{ targetSkill.caption }}",</div>
+            <div class="pl-10 grid grid-cols-[max-content,1fr] gap-x-2"><span class="">deuration: </span>{{ targetSkill.duration }},</div>
+            <div class="pl-10 grid grid-cols-[max-content,1fr] gap-x-2"><span class="">chartData: </span>{{ targetSkill.data }},</div>
+            <div><span class="text-[#42b883]">}</span></div>
         </div>
       </transition>
     </div>
@@ -46,64 +52,64 @@ dataStore.setContainerClass("container mx-auto");
 const skills = reactive([
   {
     name: "Network+",
-    caption: "lorem10",
-    position: 'row-start-1 col-start-1',
+    caption: "lorem10 Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, aut! Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, aut!",
+    position: "row-start-1 col-start-1",
     duration: 34,
     data: [100, 80, 70, 0, 30],
   },
   {
     name: "HTML",
     caption: "lorem10",
-    position: 'row-start-1 col-start-2',
+    position: "row-start-1 col-start-2",
     duration: 34,
     data: [100, 80, 70, 0, 30],
   },
   {
     name: "CSS",
     caption: "lorem10",
-    position: 'row-start-1 col-start-3',
+    position: "row-start-1 col-start-3",
     duration: 34,
     data: [100, 10, 10, 80, 90],
   },
   {
     name: "SCSS",
     caption: "lorem10",
-    position: 'row-start-1 col-start-4',
+    position: "row-start-1 col-start-4",
     duration: 34,
     data: [100, 10, 10, 0, 50],
   },
   {
     name: "JavaScript",
     caption: "lorem10",
-    position: 'row-start-1 col-start-5',
+    position: "row-start-1 col-start-5",
     duration: 34,
     data: [100, 10, 100, 0, 50],
   },
   {
     name: "JavaScript",
     caption: "lorem10",
-    position: 'row-start-1 col-start-6',
+    position: "row-start-1 col-start-6",
     duration: 34,
     data: [100, 10, 100, 0, 50],
   },
   {
     name: "Tailwind",
     caption: "lorem10",
-    position: 'row-start-2 col-start-1',
+    position: "row-start-2 col-start-1",
     duration: 34,
     data: [100, 10, 10, 0, 50],
   },
   {
     name: "CSS Grid",
-    caption: "lorem10",
-    position: 'row-start-2 col-start-2',
+    caption: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, aut! Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, aut!",
+    position: "row-start-2 col-start-2",
     duration: 34,
     data: [100, 10, 10, 0, 50],
   },
   {
     name: "CSS Grid",
-    caption: "lorem10",
-    position: 'row-start-2 col-start-3',
+    caption: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, aut! Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, aut!",
+    position: "row-start-2 col-start-3",
     duration: 34,
     data: [100, 10, 10, 0, 50],
   },
@@ -122,7 +128,11 @@ const skillIndex = (value: string) => {
 const showCaption = ref(false);
 const setData = (value: string) => {
   skillIndex(value);
-  showCaption.value = true;
+  
+    showCaption.value = false;
+  setTimeout(() => {
+    showCaption.value = true;
+  }, 400);
 };
 
 const chartData = computed(() => {
@@ -135,6 +145,9 @@ const chartData = computed(() => {
 </script>
 
 <style>
+.caption{
+  
+}
 .skill {
   /* @apply border border-[red] w-28 h-28 block hover:bg-[red] duration-500; */
   @apply bg-gray-500 text-white;
@@ -142,12 +155,23 @@ const chartData = computed(() => {
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s ease;
-  transform: rotateX(-100px);
+  transform: rotateX(-200px);
 }
 
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
   transform: rotateX(0px);
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all .4s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
