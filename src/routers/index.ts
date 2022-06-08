ca060fe2efbca65c609ae8b4d10e12d8a81f8300
Amir Maghami <amaghami@haco.dc>
2022-06-08 12:52:45 +0430

    fix: scroll top on change router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -7,6 +7,7 @@ import TheSiteContact from "../pages/site/TheSiteContact.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
   routes: [
     {
       path: "/",
@@ -36,6 +37,9 @@ const router = createRouter({
       ],
     },
   ],
+    scrollBehavior(to, from, savedPosition) {
+    return { top: 0 }
+  },
 });
 
 export default router
