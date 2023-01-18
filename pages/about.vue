90651dbaf7dac9e3a485c750b462cb5d71e5e296
Amir Maghami <Maghami.A1987@gmail.com>
2023-01-18 11:35:46 +0330

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,20 +1,29 @@
 <template>
     <div class="">
-        <h3 class="mt-12 capitalize font-bold text-lg leading-[30.24px] text-secondary">title</h3>
-        <p class="text-secondary font-normal text-base leading-[26.88px] mb-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa velit distinctio tempora fugiat recusandae
-            earum
-            illum voluptatum aliquam repudiandae accusantium quam, eum, odio quibusdam odit veritatis nesciunt.
-            Recusandae,
-            atque voluptatem.
-        </p>
-        <p class="text-secondary font-normal text-base leading-[26.88px]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa velit distinctio tempora fugiat recusandae
-            earum
-            illum voluptatum aliquam repudiandae accusantium quam, eum, odio quibusdam odit veritatis nesciunt.
-            Recusandae,
-            atque voluptatem.
-        </p>
+        <div class="">
+            <h3 class="mt-12 capitalize font-bold text-xl leading-[30.24px] text-secondary">I`m Amir Amghami</h3>
+            <div class="grid grid-cols-[1fr_max-content]">
+                <p class="text-secondary font-normal text-base leading-[26.88px] mb-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa velit distinctio tempora fugiat
+                    recusandae
+                    earum
+                    illum voluptatum aliquam repudiandae accusantium quam, eum, odio quibusdam odit veritatis nesciunt.
+                    Recusandae,
+                    atque voluptatem.
+                </p>
+                <img class="w-36" src="@/assets/img/me.png" alt="">
+            </div>
+
+            <p class="text-secondary font-normal text-base leading-[26.88px]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa velit distinctio tempora fugiat
+                recusandae
+                earum
+                illum voluptatum aliquam repudiandae accusantium quam, eum, odio quibusdam odit veritatis nesciunt.
+                Recusandae,
+                atque voluptatem.
+            </p>
+        </div>
+
     </div>
 </template>
 
