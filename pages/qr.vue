d01012bd1e3d13b47d8c3ec18f21a133097a20c4
Amir Maghami <Maghami.A1987@gmail.com>
2023-01-25 02:07:00 +0330

    feat: qr and skills page

---
+++ pages/qr.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="grid grid-cols-2 place-items-center">
+        <NuxtLink to="/" class="text-secondary">
+            <Icon name="fluent-mdl2:website" size="48" />
+        </NuxtLink>
+        <a href="tel:+989198881400" target="_blank" class="text-secondary">
+            <Icon name="ic:baseline-local-phone" size="48" />
+        </a>
+        <a href="mailto:maghami.a1987@gmail.com" target="_blank" class="text-secondary">
+            <Icon name="cib:gmail" size="48" />
+        </a>
+        
+        <a href="https://codepen.io/amirrr1987" target="_blank" class="text-secondary">
+            <Icon name="cib:codepen" size="48" />
+        </a>
+        
+        <a href="https://github.com/amirrr1987" target="_blank" class="text-secondary">
+            <Icon name="cib:github" size="48" />
+        </a>
+        
+        <a href="https://www.codewars.com/users/amirrr1987" target="_blank" class="text-secondary">
+            <Icon name="cib:codewars" size="48" />
+        </a>
+        
+        <a href="" target="_blank" class="text-secondary">
+            <Icon name="cib:skype" size="48" />
+        </a>
+        
+        <a href="" target="_blank" class="text-secondary">
+            <Icon name="cib:telegram" size="48" />
+        </a>
+        
+        <a href="https://www.linkedin.com/in/amirrr1987/" target="_blank" class="text-secondary">
+            <Icon name="cib:linkedin" size="48" />
+        </a>
+        <a href="https://stackoverflow.com/users/11471489/amirrr1987" target="_blank" class="text-secondary">
+            <Icon name="cib:stackoverflow" size="48" />
+        </a>
+    </div>
+</template>
