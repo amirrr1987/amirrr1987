c44cbf82caaf9781544978969f72a92b163186be
Amir Maghami <Maghami.A1987@gmail.com>
2024-11-10 17:59:51 +0330

    Update index.vue

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="border-b border-[#1e2d3d] py-2">
-    <Icon name="i-heroicons-solid:menu" />
+    <UButton  variant="link"  icon="i-heroicons-solid:menu" />
     <ul class="flex">
       <li v-for="link in links" :key="link.url">
         <NuxtLink
