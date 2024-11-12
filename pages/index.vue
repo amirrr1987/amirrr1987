8a2f928d9882cd67e1d18e4588e58f6d4208dd09
Amir Maghami <Maghami.A1987@gmail.com>
2024-11-12 10:51:38 +0330

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <section class="grid md:grid-cols-[288px,1fr]">
+  <section class="p-4 sm:p-6 lg:p-8">
+    <div>
+      <div>Hi all, I am</div>
+      <h1>Amir Maghami</h1>
+      <h2>Front-end developer</h2>
+      <p>
+        About me I have 5 years of experience in web development lorem ipsum
+        dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      </p>
+      const githubLink = "https://github.com/username"
+    </div>
+  </section>
+  <!-- <section class="grid md:grid-cols-[288px,1fr]">
     <div class="hidden md:block md:p-6 lg:p-8"></div>
     <div
       class="p-8 md:pl-4 md:py-12 md:pr-12 lg:py-16 lg:pr-16 grid md:grid-cols-2"
@@ -25,7 +41,7 @@
         </ClientOnly>
       </div>
     </div>
-  </section>
+  </section> -->
 </template>
 <script setup lang="ts">
 useHead({
