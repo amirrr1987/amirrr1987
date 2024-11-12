21d6745a49d28a8261ecaae9526933054572fedd
Amir Maghami <Maghami.A1987@gmail.com>
2024-11-12 15:12:34 +0330

    Update index.vue

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,40 +1,29 @@
 <template>
-  <section class="text-gray-400 grid grid-cols-[288px,1fr]">
-    <div class="p-4 sm:p-6 lg:p-8"></div>
-    <div class="py-8 pr-8 sm:py-12 sm:pr-12 lg:py-16 lg:pr-16 relative">
-      <UButton
-        variant="outline"
-        icon="i-heroicons-x-mark"
-        label="all"
-        :ui="{
-          base: 'absolute top-0 w-28',
-          rounded: 'rounded-none',
-        }"
-      />
-      <div class="grid md:grid-cols-4 gap-8 sm:gap-12 lg:gap-16">
-        <template v-for="project in projects" :key="project.id">
-          <div>
-            <UCard :ui="projectUi">
-              <template #header>
-                <NuxtImg
-                  :src="project.image"
-                  :title="project.name"
-                  :alt="project.name"
-                />
-              </template>
-              <h4 class="text-base font-medium mb-2">{{ project.name }}</h4>
-              <p>
-                {{ project.description }}
-              </p>
-              <template #footer>
-                <NuxtLink class="text-primary" :to="`/projects/${project.id}`">
-                  View Project
-                </NuxtLink>
-              </template>
-            </UCard>
-          </div>
-        </template>
-      </div>
+  <section class="text-gray-400 p-4 sm:p-6 lg:p-8">
+    <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6 lg:gap-8">
+      <template v-for="project in projects" :key="project.id">
+        <div>
+          <UCard :ui="projectUi">
+            <template #header>
+              <NuxtImg
+                :src="project.image"
+                :title="project.name"
+                :alt="project.name"
+                class="w-full"
+              />
+            </template>
+            <h4 class="text-base font-medium mb-2">{{ project.name }}</h4>
+            <p>
+              {{ project.description }}
+            </p>
+            <template #footer>
+              <NuxtLink class="text-primary" :to="`/projects/${project.id}`">
+                View Project
+              </NuxtLink>
+            </template>
+          </UCard>
+        </div>
+      </template>
     </div>
   </section>
 </template>
