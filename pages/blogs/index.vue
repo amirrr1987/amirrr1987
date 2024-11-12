4edd7a5819a84f4b9ca263297b5a42cd6a312fa4
Amir Maghami <Maghami.A1987@gmail.com>
2024-11-12 14:42:51 +0330

    Update index.vue

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,30 +1,23 @@
 <template>
-  <section class="text-gray-400 grid grid-cols-[288px,1fr]">
-    <div class="p-4 sm:p-6 lg:p-8"></div>
-    <div
-      class="py-8 pr-8 sm:py-12 sm:pr-12 lg:py-16 lg:pr-16 grid md:grid-cols-3 gap-8 sm:gap-12 lg:gap-16"
-    >
-      <template v-for="blog in data" :key="blog.id">
-        <div>
-          <UCard>
-            <template #header>
-              <img :src="blog.img" alt="" />
-            </template>
-            <h3 class="font-medium mb-2">{{ blog.title }}</h3>
-            <p class="">{{ blog.excerpt }}</p>
-
-            <template #footer>
-              <NuxtLink
-                class="text-primary"
-                :to="`blogs/${useChangeCase(blog.title, 'kebabCase').value}`"
-              >
-                View more
-              </NuxtLink>
-            </template>
-          </UCard>
-        </div>
+  <section
+    class="p-4 sm:p-6 lg:p-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 lg:gap-8"
+  >
+    <UCard v-for="blog in data" :key="blog.id">
+      <template #header>
+        <img :src="blog.img" alt="" />
       </template>
-    </div>
+      <h3 class="font-medium mb-2">{{ blog.title }}</h3>
+      <p class="">{{ blog.excerpt }}</p>
+
+      <template #footer>
+        <NuxtLink
+          class="text-primary"
+          :to="`blogs/${useChangeCase(blog.title, 'kebabCase').value}`"
+        >
+          View more
+        </NuxtLink>
+      </template>
+    </UCard>
   </section>
 </template>
 <script setup lang="ts">
