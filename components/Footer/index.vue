fb09ac25aaf9cc98cf6b83e4de34317e0dc9e0df
Amir Maghami <Maghami.A1987@gmail.com>
2024-11-12 07:58:25 +0330

    Update index.vue

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <footer class="border-t border-cool-300 dark:border-cool-800 py-2">
+  <footer class="border-t border-cool-300 dark:border-cool-700 py-2">
     <ul class="flex justify-end">
       <li>
         <NuxtLink
           to="/"
-          class="text-gray-400 border-l border-cool-300 dark:border-cool-800 px-4 py-2 flex items-center gap-2"
+          class="border-l border-cool-300 dark:border-cool-700 px-4 py-2 flex items-center gap-2"
         >
           <Icon name="tabler:brand-github" />
           <span>github</span>
