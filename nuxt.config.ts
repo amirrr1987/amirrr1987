3c3776bab4ecff1afc111e38ade1be29716acb1d
Amir Maghami <Maghami.A1987@gmail.com>
2025-06-12 02:13:06 +0330

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,37 +3,42 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 
-
-
-    modules: [
-      '@nuxt/content',
-      '@nuxt/eslint',
-      '@nuxt/fonts',
-      '@nuxt/icon',
-      '@nuxt/image',
-      '@nuxt/scripts',
-      '@nuxt/ui',
-      '@pinia/nuxt',
-      'v-gsap-nuxt',
-      '@nuxtjs/color-mode',
-      '@vueuse/nuxt'
-    ],
-    css: ['~/assets/css/main.css'],
-    app: {
-      head: {
-        title: 'Amir Maghami - Frontend Developer',
-        meta: [
-          { name: 'description', content: 'Creative modern spatial portfolio of Amir Maghami, a Frontend Developer specializing in Vue.js and Nuxt.js.' },
-          { name: 'theme-color', content: '#8B5CF6' }
-        ],
-        link: [
-          { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-          { rel: 'canonical', href: 'https://amirmaghami.ir/' }
-        ],
-        htmlAttrs: {
-          lang: 'en',
-          class: 'scroll-smooth'
-        }
+  ui: {
+    colorMode: false
+  },
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/ui',
+    '@pinia/nuxt',
+    'v-gsap-nuxt',
+    '@nuxtjs/color-mode',
+    '@vueuse/nuxt'
+  ],
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'Amir Maghami - Frontend Developer',
+      meta: [
+        {
+          name: 'description',
+          content:
+            'Creative modern spatial portfolio of Amir Maghami, a Frontend Developer specializing in Vue.js and Nuxt.js.'
+        },
+        { name: 'theme-color', content: '#8B5CF6' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'canonical', href: 'https://amirmaghami.ir/' }
+      ],
+      htmlAttrs: {
+        lang: 'en',
+        class: 'scroll-smooth'
       }
     }
-  })
+  }
+})
