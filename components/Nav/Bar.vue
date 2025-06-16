3eb4d2186d73d2ddff2ab745c7900b39c54445f2
Amir Maghami <Maghami.A1987@gmail.com>
2025-06-16 12:38:02 +0330

    Update Bar.vue

--- components/Nav/Bar.vue
+++ components/Nav/Bar.vue
@@ -1,11 +1,6 @@
 <template>
-  <UButton @click="toggleHandler" class="z-50 relative">=</UButton>
-  <UNavigationMenu
-    class="bg-red-400"
-    :class="navClass"
-    :items="navigationLinks"
-    :orientation="orientation"
-  >
+  <UButton @click="toggleHandler" class="z-50 relative md:hidden">=</UButton>
+  <UNavigationMenu :class="navClass" :items="navigationLinks" :orientation="orientation">
     <template #item="{ item }">
       <template v-if="item.to === '/'">
         <TheLogo class="h-4 w-4" />
@@ -25,17 +20,17 @@
 
   const navClass = computed(() => {
     if (isOpen.value && orientation.value === 'vertical') {
-      return 'fixed top-0 start-0 w-full h-full bg-gray-800/75 flex items-center justify-center z-50'
+      return 'fixed top-0 start-0 w-full h-full bg-slate-950/95 flex items-center justify-center z-40'
+    } else if (!isOpen.value && orientation.value === 'vertical') {
+      return 'fixed top-0 start-0 w-full h-full bg-slate-950/95 flex items-center justify-center z-40 hidden'
     } else if (isOpen.value && orientation.value === 'horizontal') {
-      return 'fixed top-0 left-0 w-full bg-gray-800/75 flex items-center justify-center z-50'
+      return 'fixed top-0 left-0 w-full bg-slate-950/95 flex items-center justify-center z-40'
     } else {
-      return 'hidden'
+      return 'relative'
     }
   })
   const toggleHandler = () => {
-    console.log('🚀 ~ toggleHandler ~ isOpen.value:', isOpen.value)
     isOpen.value = !isOpen.value
-    console.log('🚀 ~ toggleHandler ~ isOpen.value:', isOpen.value)
   }
   const navigationLinks = [
     { label: 'Home', to: '/', icon: 'i-heroicons-home' },
